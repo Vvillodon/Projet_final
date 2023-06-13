5363de69ab229768a6ca64f99d86b6902291105d
Vvillodon <133153983+Vvillodon@users.noreply.github.com>
2023-06-13 15:00:17 -0400

    affichage

---
+++ BlueOrigin/affichage_resultats/affichage.py
@@ -0,0 +1,17 @@
+'''Liste des fonctions 
+affichage de la trajectoire 
+affichage du plan de vol 
+affichage physique 
+'''
+
+class affichage():
+    def __init__():
+        pass
+    
+    def affichage_trajectoire():
+        pass
+    
+    def affichage_plan_de_vol():
+        pass
+    
+    def affichage_physique():
