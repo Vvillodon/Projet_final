69b4e07332db4e2622e0a707f0906ef0e07f0e23
Vvillodon <133153983+Vvillodon@users.noreply.github.com>
2023-06-13 17:28:36 -0400

    Calcul vent

--- BlueOrigin/conditions_entrees/profil_vent.py
+++ BlueOrigin/conditions_entrees/profil_vent.py
@@ -7,22 +7,40 @@ class Profil_vent():
         pass
     
     def calcul_vent(self,altitude_minimal ,altitude_maximal,vitesse_sol, rugosite):
+        """
+        
+
+        Parameters
+        ----------
+        altitude_minimal : TYPE
+            DESCRIPTION.
+        altitude_maximal : TYPE
+            DESCRIPTION.
+        vitesse_sol : TYPE
+            DESCRIPTION.
+        rugosite : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        result : TYPE
+            DESCRIPTION.
+
+        """
         z0 = rugosite
         h1 = altitude_minimal
         h2 = altitude_maximal
         V1 = vitesse_sol
 
-        with open('vitesse_vent.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Altitude (m)', 'Vitesse Vent (m/s)'])
+        result = {}
 
-            for altitude in range(h1, h2 + 1):
-                vitesse_vent = V1 * (math.log(altitude/z0)) / (math.log(h1/z0))
-                writer.writerow([altitude, vitesse_vent])
-
-        print("Le fichier CSV 'vitesse_vent.csv' a été généré avec succès.")
+        for altitude in range(h1, h2 + 1):
+            vitesse_vent = V1 * (math.log(altitude/z0)) / (math.log(h1/z0))
+            result[altitude] = vitesse_vent
+            
+        return result
         
     
 
 
-Profil_vent().calcul_vent(100, 2000, 10, 0.1)
+print(Profil_vent().calcul_vent(100, 2000, 10, 0.1))
