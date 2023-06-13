7394145345443a2a8953875201bede5f5078802b
AFarina6393 <alice.farina63540@gmail.com>
2023-06-13 15:03:50 -0400

    update physique_vol.py

---
+++ BlueOrigin/analyse_donnees/physique_vol.py
@@ -0,0 +1,29 @@
+""" DESCRIPTION FICHIER """
+
+class PhysiqueVol():
+    g=9.81
+    isp=0
+    masse_vide_fusee=0
+
+    def __init__(self, masse_payload):
+        self.masse_payload = masse_payload
+
+    def calcul_vitesse(self):
+        pass
+
+    def calcul_masse(self):
+        pass
+
+    def calcul_poussee(self):
+        pass
+
+    def calcul_altitude(self):
+        pass
+
+    def calcul_centre_gravite(self):
+        pass
+
+class Trajectoire(PhysiqueVol):
+    def __init__(self):
+        super().__init__(masse_payload)
+        
