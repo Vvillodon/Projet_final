4e5a240728f3746565ab7016c099b54d361ea896
AFarina6393 <alice.farina63540@gmail.com>
2023-06-14 16:25:45 -0400

    Modifications alice

--- BlueOrigin/analyse_donnees/plan_vol.py
+++ BlueOrigin/analyse_donnees/plan_vol.py
@@ -3,5 +3,11 @@
 
 class PlanVol():
 
-    def __init__(self):
-        
+    def __init__(self,
+                 data : list):
+        self.data = data
+
+    def creer_plan_vol(self):
+        plan_vol_final = dict()
+
+        return plan_vol_final
