c6add87e83489fcf060ae553804eee64669aa625
nbazetoux <noe.bazetoux@gmail.com>
2023-06-14 16:41:10 -0400

    maj cartesian

--- BlueOrigin/pretraitement/creation_nv_fichier.py
+++ BlueOrigin/pretraitement/creation_nv_fichier.py
@@ -1,4 +1,6 @@
 import csv
+import math
+
 from pyproj import CRS, Transformer
 
 class DataProcessor:
@@ -7,11 +9,6 @@ class DataProcessor:
         self.data = self.read_csv_file()
 
     def read_csv_file(self):
-        """
-        Lit un fichier CSV et retourne les données sous forme de liste de dictionnaires.
-
-        :return: Les données du fichier CSV sous forme de liste de dictionnaires.
-        """
         data = []
         with open(self.filename, "r") as csvfile:
             reader = csv.DictReader(csvfile)
@@ -20,39 +17,34 @@ class DataProcessor:
         return data
 
     def write_csv_file(self, filename, data, fieldnames):
-        """
-        Écrit les données dans un fichier CSV.
-
-        :param filename: Le nom du fichier CSV de sortie.
-        :param data: Les données à écrire dans le fichier CSV.
-        :param fieldnames: Les noms de colonnes du fichier CSV.
-        """
         with open(filename, "w", newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(data)
 
-    def convert_ecef_to_lla(self):
-        """
-        Convertit les positions du repère ECEF vers le repère LLA (Latitude, Longitude, Altitude).
-
-        """
-        transformer = Transformer.from_crs(CRS.from_epsg(4978), CRS.from_epsg(4326))
+    def convert_ecef_to_cartesian(self):
         for row in self.data:
-            pos_X = float(row["pos_X"])
-            pos_Y = float(row["pos_Y"])
-            pos_Z = float(row["pos_Z"])
-            lon, lat, alt = transformer.transform(pos_X, pos_Y, pos_Z)
-            row["Latitude"] = lat
-            row["Longitude"] = lon
-            row["Altitude"] = alt
+            pos_x = float(row["pos_X"])
+            pos_y = float(row["pos_Y"])
+            pos_z = float(row["pos_Z"])
+
+            a = 6372368.0  # Demi-grand axe de la Terre (m)
+
+            # Calcul des coordonnées cartésiennes
+            r = math.sqrt(pos_x ** 2 + pos_y ** 2 + pos_z ** 2)
+            theta = math.acos(pos_z / r)
+            phi = math.atan2(pos_y, pos_x)
+
+            # Conversion en coordonnées cartésiennes sur Terre
+            z_cartesian = r - a
+            x_cartesian = theta * a - 6533890.282256417
+            y_cartesian = phi * a - -11650966.007387657
+
+            row["x_cartesian"] = x_cartesian
+            row["y_cartesian"] = y_cartesian
+            row["z_cartesian"] = z_cartesian
 
     def filter_and_rename_columns(self):
-        """
-        Filtre les colonnes et les renomme dans les données.
-
-        :return: Les données avec les colonnes filtrées et renommées.
-        """
         filtered_data = []
         for row in self.data:
             filtered_row = {
@@ -68,14 +60,8 @@ class DataProcessor:
         self.data = filtered_data
 
     def process_data(self, output_filename):
-        """
-        Effectue le traitement des données, y compris le filtrage, la conversion ECEF vers LLA et le renommage des colonnes.
-        Écrit les données traitées dans un nouveau fichier CSV.
-
-        :param output_filename: Le nom du fichier CSV de sortie.
-        """
         self.filter_and_rename_columns()
-        self.convert_ecef_to_lla()
+        self.convert_ecef_to_cartesian()
 
         fieldnames = self.data[0].keys()
         self.write_csv_file(output_filename, self.data, fieldnames)
