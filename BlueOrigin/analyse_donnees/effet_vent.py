d728a7016ae6d701f2049746171e8b007231334c
Vvillodon <133153983+Vvillodon@users.noreply.github.com>
2023-06-14 21:53:12 -0400

    add

---
+++ BlueOrigin/analyse_donnees/effet_vent.py
@@ -0,0 +1,12 @@
+
+
+
+class Effet_vent():
+    
+    
+    def __init__(self):
+        pass
+    def force_vent(self):
+        pass
+    def decalage(self):
+        pass
--- BlueOrigin/analyse_donnees/physique_vol.py
+++ BlueOrigin/analyse_donnees/physique_vol.py
@@ -6,7 +6,7 @@ from .plan_vol import PlanVol
 
 class PhysiqueVol(PlanVol):
     g = 9.81
-    isp = 260
+    isp = 315
     masse_vide_fusee = 20569 # kg
     masse_pleine_fusee = 75000 # kg
     diametre_fusee = 7 # m
@@ -14,7 +14,7 @@ class PhysiqueVol(PlanVol):
 
     def __init__(self,
                  masse_payload : int):
-        PlanVol.__init__(data)  # [[t, pos_x, pos_y, pos_z, vx, vy, vz, x_cart, y_cart, z_cart], [ti+1, ...],...]
+        PlanVol.__init__(self.data)  # [[t, pos_x, pos_y, pos_z, vx, vy, vz, x_cart, y_cart, z_cart], [ti+1, ...],...]
         self.masse_payload = masse_payload
         self.plan_vol = self.creer_plan_vol()
 
@@ -63,3 +63,4 @@ class PhysiqueVol(PlanVol):
 
     def calcul_centre_gravite(self):
         pass
+    
