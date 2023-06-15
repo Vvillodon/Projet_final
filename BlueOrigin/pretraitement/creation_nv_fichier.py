fc67f3cd9cbdbc5db1eb94047ea93938a30a6db8
nbazetoux <noe.bazetoux@gmail.com>
2023-06-15 19:59:01 -0400

    debug

--- BlueOrigin/pretraitement/creation_nv_fichier.py
+++ BlueOrigin/pretraitement/creation_nv_fichier.py
@@ -1,7 +1,6 @@
 import csv
 import math
-from Projet_final.globs import (CSV_FILTERED_PATH_DEFAULT,
-                                FINAL_CSV_FILTERED_PATH_DEFAULT)
+from globs import (CSV_FILTERED_PATH_DEFAULT, FINAL_CSV_FILTERED_PATH_DEFAULT)
 
 
 class DataProcessor:
