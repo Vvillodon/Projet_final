e73db8cbe78c5b98a57face58bb75d6e1e2d8552
Vvillodon <133153983+Vvillodon@users.noreply.github.com>
2023-06-15 22:25:59 -0400

    plan de vol

--- BlueOrigin/analyse_donnees/plan_vol.py
+++ BlueOrigin/analyse_donnees/plan_vol.py
@@ -1,11 +1,38 @@
-import math
+from math import *
 import numpy as np
 
 class PlanVol():
 
-    def __init__(self,
-                 data : list):
+    def __init__(self,data : list):
         self.data = data
+        self.time = [colonne[0] for colonne in self.data]
+        
+        self.vx_ecef = [colonne[4] for colonne in self.data]
+        self.vy_ecef = [colonne[5] for colonne in self.data]
+        self.vz_ecef = [colonne[6] for colonne in self.data]
+        
+        self.x_cartesian = [colonne[7] for colonne in self.data]
+        self.y_cartesian = [colonne[8] for colonne in self.data]
+        self.z_cartesian = [colonne[9] for colonne in self.data]
+        
+        
+        
+    def calcul_valeurs_normees(self):
+        """
+        Calcule la norme des vitesses vx, vy, vz et la norme des altitudes en coordonées cartésiennes x_cart, y_cart,
+         z_cart pour chaque instant t.
+        L'origine du repère cartésiens se trouve au pas de tir du lancement de la fusée.
+
+        :return: liste_valeurs_normees : liste de listes contenant chaque temps d'échantillonage et altitude et vitesse
+        normées respectives [[to, ho, vo], [t1, h1, v1],...]
+        """
+
+
+        altitude_normee = np.sqrt(self.x_cartesian**2+self.y_cartesian**2+self.z_cartesian**2)
+        vitesse_normee = np.sqrt(self.vx_ecef**2+self.vy_ecef**2+self.vz_ecef**2)
+
+        return altitude_normee,vitesse_normee
+                 
 
     def creer_plan_vol(self):
         
@@ -19,41 +46,44 @@ class PlanVol():
         
         altitude_liftoff=self.data[1][9]
         i=2
+        
+        altitude_normee,vitesse_normee=calcul_valeurs_normees(self)
+        
+        
         while tp_liftoff==0 or tp_MECO==0 or tp_apogee==0 or tp_deploy_brakes==0 or tp_restart_ignition==0 or tp_touchdown==0:
-            altitude=self.data[i][9]
-            altitude_before=self.data[i][9]
-            deltaV=np.linalg.norm([self.data[i][4],self.data[i][5],self.data[i][6]])-np.linalg.norm([self.data[i-1][4],self.data[i-1][5],self.data[i-1][6]])
-            deltaV_before=np.linalg.norm([self.data[i-1][4],self.data[i-1][5],self.data[i-1][6]])-np.linalg.norm([self.data[i-2][4],self.data[i-2][5],self.data[i-2][6]])
+            altitude=self.z_cartesian[i]
+            altitude_before=self.z_cartesian[i]
+            
+            deltaV=vitesse_normee[i]-vitesse_normee[i-1]
+            deltaV_before=vitesse_normee[i-1]-vitesse_normee[i-2]
 
             if tp_liftoff==0 and altitude-altitude_liftoff>=1: 
-                tp_liftoff=self.data[i][0]
+                tp_liftoff=self.time[i]
                 
             elif tp_liftoff!=0 and tp_MECO==0 and -9.81<deltaV<-9.5:
-                tp_MECO=self.data[i][0]
+                tp_MECO=self.time[i]
                 
             elif tp_MECO!=0 and tp_apogee==0 and np.sign(altitude-altitude_before)==-1:
-                tp_apogee=self.data[i][0]
+                tp_apogee=self.time[i]
                 
             elif  tp_apogee!=0 and tp_deploy_brakes==0 and np.sign(deltaV-deltaV_before)==-1 and -10<deltaV<0:
-                tp_deploy_brakes=self.data[i][0]
+                tp_deploy_brakes=self.time[i]
                 
             elif tp_deploy_brakes!=0 and tp_restart_ignition==0 and np.sign(deltaV-deltaV_before)==-1 and -10<deltaV<0:
-                tp_deploy_brakes=self.data[i][0]
+                tp_deploy_brakes=self.time[i]
                 
             elif tp_restart_ignition!=0 and tp_touchdown==0 and altitude-altitude_liftoff<=1:
-                tp_touchdown=self.data[i][0]
+                tp_touchdown=self.time[i]
             
             i+=1
-        
-            
-        
-        
-        
         
         
         
         
         plan_vol_final = {'ignition':tp_ignition,'liftof':tp_liftoff,'MECO':tp_MECO,'apogee':tp_apogee,'deploy_brake':tp_deploy_brakes,'restart ignition':tp_restart_ignition,'touch down':tp_touchdown}
-        
+        print(plan_vol_final)
 
         return plan_vol_final
+    
+    
+    
